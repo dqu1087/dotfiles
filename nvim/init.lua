c44c4484cd201829fb1245133053059f257c0b8c
Devin Qu <dqu1087@gmail.com>
2023-11-29 16:54:37 -0500

    initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,120 @@
+vim.g.mapleader = ' '
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+      }
+    },
+    branch = "0.1.x"
+  },
+
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("kanagawa").setup({
+        commentStyle = { italic = false },
+        keywordStyle = { italic = false }
+      })
+
+      vim.cmd.colorscheme("kanagawa")
+    end
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate"
+  },
+
+  { "neovim/nvim-lspconfig" },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip"
+    }
+  }
+})
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.keymap.set("n", "<Leader>y", ":%y+<CR>")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+require("nvim-treesitter.configs").setup({
+  auto_install = true,
+  highlight = {enable = true }
+})
+
+require("telescope").load_extension("fzf")
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<Leader>sf", builtin.find_files)
+
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local servers = { "clangd", "pyright" }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup({
+    capabilities = capabilities
+  })
+end
+
+for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+  vim.api.nvim_set_hl(0, group, {})
+end
+
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
+
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p"] = cmp.mapping.select_prev_item(),
+    ["<C-n"] = cmp.mapping.select_next_item(),
+    ["<C-u"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d"] = cmp.mapping.scroll_docs(4)
+  }),
+
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "nvim_lsp_signature_help" },
+    { name = "luasnip" }
+  })
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  end
+})
